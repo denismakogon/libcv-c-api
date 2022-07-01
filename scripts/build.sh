af67ddba3a341ca2c09856d67ff47400405df220
Denis Makogon <denys.makogon@oracle.com>
2022-07-01 13:13:34 +0300

    build on macos only

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,8 @@ mkdir -p "${location}/lib"
 # shellcheck disable=SC2046
 g++ -I src/ $(pkg-config --cflags --libs opencv4) \
     -std=c++11  -dynamiclib src/*.cpp \
-    -o "${location}/lib/libopencv_c_api-${version}.dylib"
+    -o "${location}/lib/libopencv_c_api-${version}.dylib" \
+    -current_version "${version}" -compatibility_version "${version}"
 
 cp "src/include/c_api.h" "${location}/include/c_api.h"
 cp "src/include/data_types.h" "${location}/include/data_types.h"
