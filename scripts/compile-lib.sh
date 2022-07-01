b461eea8f764d1b1c3f74b26da2f2f404824d566
Denis Makogon <denys.makogon@oracle.com>
2022-07-01 14:42:11 +0300

    release binary if possible

--- scripts/compile-lib.sh
+++ scripts/compile-lib.sh
@@ -4,12 +4,14 @@ set -xe
 
 version=${1:-"$(date +'%Y.%m.%d')"}
 build_dir=${2:-"build"}
+platform="linux"
+arch=$(uname -m)
 lib_extension="so"
 if [[ $OSTYPE == 'darwin'* ]]; then
   lib_extension="dylib"
+  platform="macos"
 fi
-
-libname="libopencv_c_api-${version}.${lib_extension}"
+libname="libopencv_c_api-${version}.${platform}.${arch}.${lib_extension}"
 
 rm -fr "${build_dir:?}/*"
 mkdir -p "${build_dir}/include"
--- scripts/package-lib.sh
+++ scripts/package-lib.sh
@@ -4,8 +4,16 @@ set -xe
 
 version=${1:-"$(date +'%Y.%m.%d')"}
 build_dir=${2:-"build"}
+platform="linux"
+arch=$(uname -m)
+lib_extension="so"
+if [[ $OSTYPE == 'darwin'* ]]; then
+  lib_extension="dylib"
+  platform="macos"
+fi
+libname="libopencv_c_api-${version}.${platform}.${arch}.${lib_extension}"
 
 /bin/bash scripts/compile-lib.sh "${version}" "${build_dir}"
 
-pushd build && tar -zcvf "/tmp/libopencv_c_api.dylib-${version}.tar.gz" include lib; popd
-mv "/tmp/libopencv_c_api.dylib-${version}.tar.gz" "${build_dir:?}/libopencv_c_api.dylib-${version}.tar.gz"
+pushd build && tar -zcvf "/tmp/${libname}" include lib; popd
+mv "/tmp/${libname}" "${build_dir:?}/${libname}"
